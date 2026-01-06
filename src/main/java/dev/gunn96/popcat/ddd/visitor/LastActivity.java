c9d3906f292b7918fa156667b9e387df4fd22670
gggeon96 <gggeon96@github.com>
2026-01-06 17:23:12 +0900

    update(visitor): change recordClick method
     - 시간을 파라미터로 받아서 테스트 용이하도록 변경

--- src/main/java/dev/gunn96/popcat/ddd/visitor/LastActivity.java
+++ src/main/java/dev/gunn96/popcat/ddd/visitor/LastActivity.java
@@ -1,8 +1,19 @@
 package dev.gunn96.popcat.ddd.visitor;
 
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Builder
+@RequiredArgsConstructor
 public class LastActivity {
-    private LocalDateTime timestamp;
+    private final LocalDateTime timestamp;
+
+    protected LastActivity updateLastActivity(LocalDateTime localDateTime) {
+        return LastActivity.builder()
+                .timestamp(localDateTime)
+                .build();
+    }
 
 }
