a0aea3af4ab9776d984d88ebbe7c80981cbb0602
rachaen <rachaenlee@gmail.com>
2026-01-08 18:00:48 +0900

    init frontend project

---
+++ alpopca-front/src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './assets/alpaca.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>ALPOPCA</h1>
+      <div>
+          <img src={logo} className="logo react" alt="React logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
